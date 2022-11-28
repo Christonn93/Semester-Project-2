9be84d73d9db624568bb997703accddcb47cacdb
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-28 13:15:45 +0100

    layout changes for mobile

--- src/js/ui/displayListing.mjs
+++ src/js/ui/displayListing.mjs
@@ -9,19 +9,18 @@ export async function displayListingUi() {
   data.forEach((el) => {
     const { title, description, tags: tags = [], media, endsAt, id, updated, created, _count, seller, bids } = el;
     const { name: sellerName } = seller;
-
-    console.log(el);
     // Switch for displaying cards
     const routeName = document.body.id;
     switch (routeName) {
       // Display only user listings
       case 'dashboard':
         if (!userToken) {
-          const userName = JSON.parse(localStorage.getItem('Username'));
-          if (userName == sellerName) {
-            listingItemsList.append(displayListingFactory('div', 'card,ac-listing-item', `listingId=${id}`, media, title, description, tags, endsAt, id));
-          }
           window.location.replace('../../../index.html');
+        }
+        
+        const userName = JSON.parse(localStorage.getItem('Username'));
+        if (userName == sellerName) {
+          listingItemsList.append(displayListingFactory('div', 'card,ac-listing-item', `listingId=${id}`, media, title, description, tags, endsAt, id));
         }
         break;
 
