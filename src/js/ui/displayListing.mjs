4158f639c1d0d5002e8c92721c3c242ad26e3dfe
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-28 13:01:09 +0100

    Adjusting logo for mobile

--- src/js/ui/displayListing.mjs
+++ src/js/ui/displayListing.mjs
@@ -2,31 +2,32 @@ import { getListings } from '../api/user/listings/listings.js';
 import { displayListingFactory } from '../tools/displayListingFactory.js';
 const data = await getListings();
 
-const userToken = localStorage.getItem("Token")
+const userToken = localStorage.getItem('Token');
+const listingItemsList = document.querySelector('#listingItems');
 
 export async function displayListingUi() {
   data.forEach((el) => {
     const { title, description, tags: tags = [], media, endsAt, id, updated, created, _count, seller, bids } = el;
     const { name: sellerName } = seller;
 
-    const listingItemsList = document.querySelector('#listingItems');
-
+    console.log(el)
+    // Switch for displaying cards
     const routeName = document.body.id;
     switch (routeName) {
       // Display only user listings
       case 'dashboard':
-        const userName = JSON.parse(localStorage.getItem('Username'))
+        const userName = JSON.parse(localStorage.getItem('Username'));
         if (userName == sellerName) {
-          listingItemsList.append(displayListingFactory("div", "card,ac-listing-item", `listingId=${id}`, media, title, description, tags, endsAt, id)) 
+          listingItemsList.append(displayListingFactory('div', 'card,ac-listing-item', `listingId=${id}`, media, title, description, tags, endsAt, id));
         }
-        if(!userToken){
-          window.location.replace("../../../index.html")
+        if (!userToken) {
+          window.location.replace('../../../index.html');
         }
         break;
 
       case 'homepage':
-        if (tags[0] == "Car") {
-          listingItemsList.append(displayListingFactory("div", "card,ac-listing-item", `listingId=${id}`, media, title, description, tags, endsAt, id)) 
+        if (tags[0] == 'Car') {
+          listingItemsList.append(displayListingFactory('div', 'card,ac-listing-item', `listingId=${id}`, media, title, description, tags, endsAt, id));
         }
     }
   });
