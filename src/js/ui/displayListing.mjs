988d2acbd94844802e4cb946cec8b8ed6081c054
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-28 13:01:25 +0100

    Adjusting logo for mobile

--- src/js/ui/displayListing.mjs
+++ src/js/ui/displayListing.mjs
@@ -10,17 +10,17 @@ export async function displayListingUi() {
     const { title, description, tags: tags = [], media, endsAt, id, updated, created, _count, seller, bids } = el;
     const { name: sellerName } = seller;
 
-    console.log(el)
+    console.log(el);
     // Switch for displaying cards
     const routeName = document.body.id;
     switch (routeName) {
       // Display only user listings
       case 'dashboard':
-        const userName = JSON.parse(localStorage.getItem('Username'));
-        if (userName == sellerName) {
-          listingItemsList.append(displayListingFactory('div', 'card,ac-listing-item', `listingId=${id}`, media, title, description, tags, endsAt, id));
-        }
         if (!userToken) {
+          const userName = JSON.parse(localStorage.getItem('Username'));
+          if (userName == sellerName) {
+            listingItemsList.append(displayListingFactory('div', 'card,ac-listing-item', `listingId=${id}`, media, title, description, tags, endsAt, id));
+          }
           window.location.replace('../../../index.html');
         }
         break;
