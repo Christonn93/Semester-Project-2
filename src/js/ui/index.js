b3166bc3072f9765bc7bdf47f7dc69e4ce0cd401
Christopher <46559129+Christonn93@users.noreply.github.com>
2022-11-30 12:21:59 +0100

    User/update avatar (#6)
    * 1.1.5
    
    * Working on form to change the user avatar
    HAving some issues whit updating the page whit new content

--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -1,6 +1,11 @@
-import { displayListingUi } from './displayListing.mjs';
+export * from './dashboardDisplay';
+export * from './displayListing';
+export * from './supporter';
+export * from './userOptionBtn';
+
+import { displayListingUi } from './displayListing.js';
 import { displayProfileInformation } from './dashboardDisplay.js';
-import { displayLoggedInButtons } from './userOptionBtn.mjs';
+import { displayLoggedInButtons } from './userOptionBtn.js';
 displayListingUi();
 displayProfileInformation();
 displayLoggedInButtons();
