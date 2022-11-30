767e91feec6c3badbd11bfda1950eabbb9ee03a8
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-30 10:34:07 +0100

    Adding a listener for creating new item. moving some code from one place to where it need to be. Connecting listener to form for creating new items

---
+++ src/js/listeners/newEntery.js
@@ -0,0 +1,17 @@
+import { createNewEntry } from '../api/user/listings/createNew';
+
+const form = document.querySelector('#newEntryForm');
+if (form) {
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+    const newData = Object.fromEntries(formData.entries());
+    console.log(newData);
+    const { title, description, tags, endsAt, media } = newData;
+    // send it to API
+    await createNewEntry(title, description, [media], [tags], endsAt);
+    window.location.replace('../');
+  });
+}
