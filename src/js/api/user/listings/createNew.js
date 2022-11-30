80a6495ccf209955ea80fb3462c794c27031c7b8
Christopher <46559129+Christonn93@users.noreply.github.com>
2022-11-30 12:18:32 +0100

    User/new entry item (#4)
    * 1.1.5
    
    * Adding a listener for creating new item. moving some code from one place to where it need to be. Connecting listener to form for creating new items

--- src/js/api/user/listings/createNew.js
+++ src/js/api/user/listings/createNew.js
@@ -1,38 +1,23 @@
 import * as url from '../../constant.js';
 
-const form = document.querySelector('#newEntryForm');
-if (form) {
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
+export async function createNewEntry(title, description, media, tags, endsAt) {
+  const token = JSON.parse(localStorage.getItem('Token'));
 
-    const form = event.target;
-    const formData = new FormData(form);
-    const newData = Object.fromEntries(formData.entries());
-    console.log(newData);
-    const { title, description, tags, endsAt, media } = newData;
-    // send it to API
-    createNewEntry(title, description, [media], [tags], endsAt);
-  });
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ title, description, media, tags, endsAt }),
+  };
 
-  async function createNewEntry(title, description, media, tags, endsAt) {
-    const token = JSON.parse(localStorage.getItem('Token'));
-
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ title, description, media, tags, endsAt }),
-    };
-
-    try {
-      const req = await fetch(url.api_base_url + url.createNewListingEndPoint, options);
-      if (req.ok) {
-        return await req.json();
-      }
-    } catch {
-      // Show user a message that they couldn't log in
+  try {
+    const req = await fetch(url.api_base_url + url.createNewListingEndPoint, options);
+    if (req.ok) {
+      return await req.json();
     }
+  } catch {
+    // Show user a message that they couldn't log in
   }
 }
