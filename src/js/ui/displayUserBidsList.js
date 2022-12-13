0c87823185f38ae272f090b96fb49e904b4fdfc3
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-12-13 09:43:08 +0100

    removing console log

--- src/js/ui/displayUserBidsList.js
+++ src/js/ui/displayUserBidsList.js
@@ -3,24 +3,14 @@ import { changeTimeFormat } from '../tools/time/changeTime.js';
 
 const parentElement = document.getElementById('placedBids');
 
-console.log(parentElement);
-
 export async function displayUserBids() {
   const data = await profileBidsFetch();
 
   data.forEach((bid) => {
     const { amount, listing } = bid;
-    const { title, id, endsAt, media: media = [] } = listing;
+    const { title, id, endsAt } = listing;
 
     const newTime = changeTimeFormat(endsAt);
-
-    let displayMedia = media[0];
-
-    if (media.length === 0) {
-      displayMedia = 'https://png.pngitem.com/pimgs/s/287-2876527_uncle-mike-s-qd115-ns-circle-hd-png.png';
-    }
-
-    console.log(displayMedia);
 
     let items = document.createElement('div');
     items.classList.add('container', 'card', 'shadow', 'bg-theme-beige', 'flex-lg-row', 'p-2', 'gap-2', 'align-items-center', 'flex-sm-column', 'flex-md-column');
