e459e794e2b2afee234c787d49fa0810e72170cb
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-12-14 11:04:51 +0100

    Dealing with issues displaying bids on profile site.

---
+++ src/js/classes/bidClass.js
@@ -0,0 +1,9 @@
+export class BidListing {
+  constructor(title, id, amount, url, time) {
+    this.title = title;
+    this.id = id;
+    this.amount = amount;
+    this.url = url;
+    this.time = time;
+  }
+}
