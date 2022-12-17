1eb052b56b0de2d97cde885844638f22f18149f3
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-12-17 17:05:11 +0100

    Working on bid displaying

--- src/js/ui/dashboard/displayUserBidsList.js
+++ src/js/ui/dashboard/displayUserBidsList.js
@@ -1,5 +1,5 @@
 import { profileBidsFetch } from '../../api/fetch/profileBidsFetch.js';
-import { changeTimeFormat } from '../../tools/time/changeTime.js';
+import { bidCard } from '../ui-elements/bidsCards.js';
 
 const parentElement = document.getElementById('placedBids');
 const parentElementEnded = document.getElementById('placedBidsEnded');
@@ -11,55 +11,31 @@ const parentElementEnded = document.getElementById('placedBidsEnded');
 export async function displayUserBids() {
   const data = await profileBidsFetch();
 
-  for (let i = 0; i < data.length; i++) {
-    const listingDetails = data[i].listing;
-
-    for (let i = 0; i < listingDetails.length; i++) {
-      console.log(listingDetails[i].title);
-    }
+  // Checking if there is items. If it is remove the HTML element
+  if (data) {
+    document.getElementById('empty-bids').classList.add('d-none');
+    document.getElementById('empty-bidsEnded').classList.add('d-none');
   }
 
-  data.forEach((bid) => {
-    const { amount, listing, created } = bid;
-    const { title, id, endsAt } = listing;
-    let timeAdded = changeTimeFormat(created);
+  window.bids = data;
 
-    let time = changeTimeFormat(endsAt);
-    const itemDate = new Date(time);
-    const todayDate = new Date();
+  renderBids(data);
 
-    if (itemDate <= todayDate) {
-      time = `<i class="fa-solid fa-exclamation"></i> Auction ended`;
-    }
+  function renderBids(data) {
+    data.forEach((e) => {
+      let bids = bidCard(e);
+      const itemDate = new Date(e.endsAt);
+      const todayDate = new Date();
 
-    let items = document.createElement('div');
-    items.classList.add('container', 'card', 'shadow', 'bg-theme-beige', 'flex-lg-row', 'p-2', 'gap-2', 'align-items-center', 'flex-sm-column', 'flex-md-column', 'bid-info');
-    items.id = id;
-    items.dataset.created = time;
-    items.dataset.amount = amount;
-    items.innerHTML = `<div class="container">
-    <h4>${title}</h4>
-  </div>
-  <div class="container-fluid d-flex flex-column gap-1">
-  <span>Bid amount: ${amount} <i class="fa-solid fa-coins"></i></span>
-  <span>Bid added: ${timeAdded}</span>
-  <span>Ending at: ${time}</span>
-  </div>
-  <div class="container d-flex justify-content-end">
-    <a href="/pages/listings/listing-item/index.html?id=${id}" class="btn btn-theme-blue text-center shadow">View item</a>
-  </div>`;
+      if (itemDate <= todayDate) {
+        parentElementEnded.innerHTML += bids;
+      } else {
+        parentElement.innerHTML += bids;
+      }
+    });
+  }
 
-    // Checking if listing har ended
-    if (itemDate <= todayDate) {
-      parentElementEnded.append(items);
-    } else {
-      parentElement.append(items);
-    }
-
-    // Checking if there is items. If it is remove the HTML element
-    if (items) {
-      document.getElementById('empty-bids').classList.add('d-none');
-      document.getElementById('empty-bidsEnded').classList.add('d-none');
-    }
-  });
+  // function updateBid(bids) {
+  //   const updatedBids = window.bids.map(...renderBids(updatedBids));
+  // }
 }
---
+++ src/js/ui/ui-elements/bidsCards.js
@@ -0,0 +1,25 @@
+import { changeTimeFormat } from '../../tools/time/changeTime';
+
+export const bidCard = (data) => {
+  let { amount, listing } = data;
+  let { title, id, endsAt } = listing;
+
+  let time = changeTimeFormat(endsAt);
+  let itemDate = new Date(time);
+  let todayDate = new Date();
+
+  if (itemDate <= todayDate) {
+    time = `<i class="fa-solid fa-exclamation"></i> Auction ended`;
+  }
+
+  return `<div class="container card shadow bg-theme-beige flex-lg-row p-2 gap-2 align-items-center flex-sm-column flex-md-column bid-info" id="${id}">
+<div class="container"><h4>${title}</h4></div>
+<div class="container-fluid d-flex flex-column gap-1">
+<span>Bid amount: ${amount} <i class="fa-solid fa-coins"></i></span>
+<span>Ending at: ${time}</span>
+</div>
+<div class="container d-flex justify-content-end">
+<a href="/pages/listings/listing-item/index.html?id=${id}" class="btn btn-theme-blue text-center shadow">View item</a>
+</div>
+</div>`;
+};
