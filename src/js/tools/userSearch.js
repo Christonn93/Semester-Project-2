74073c897a155cdc5eca02dd8ba13ce2dd0629a2
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-12-18 11:19:22 +0100

    Changing the search function

--- src/js/tools/userSearch.js
+++ src/js/tools/userSearch.js
@@ -3,32 +3,24 @@ import { getListings } from '../api/fetch/listings.js';
 // Selecting html element for use in search function
 const searchInput = document.getElementById('search');
 
+// Retrieving array from fetch & adding tag
+const data = await getListings();
+console.log(data);
 /**
  * A search function that reacts on user input in search field on page.
  */
 export async function userSearch() {
   if (searchInput) {
-    // Retrieving array from fetch & adding tag
-    const tag = ['car', 'Car'];
-    const data = await getListings(tag);
     searchInput.addEventListener('input', (e) => {
       const value = e.target.value.toLowerCase();
       data.forEach((item, i) => {
         const listingItemsList = document.getElementById('listingItems');
         const listItems = listingItemsList.children[i];
         let isVisible;
-        if (item.description !== null) {
-          isVisible = item.title.toLowerCase().includes(value) || item.description.toLowerCase().includes(value);
-          if (listItems) {
-            listItems.classList.toggle('d-none', !isVisible);
-            document.querySelector('.loader').classList.add('d-none');
-          }
-        } else {
-          isVisible = item.title.toLowerCase().includes(value);
-          if (listItems) {
-            listItems.classList.toggle('d-none', !isVisible);
-            document.querySelector('.loader').classList.add('d-none');
-          }
+        isVisible = item.title.toLowerCase().includes(value);
+        if (listItems) {
+          listItems.classList.toggle('d-none', !isVisible);
+          document.querySelector('.loader').classList.add('d-none');
         }
       });
     });
